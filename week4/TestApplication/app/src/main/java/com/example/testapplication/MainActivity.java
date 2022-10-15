6a71931a780c1377f46abc08ef77b4d01977b6b1
U190709045 <mehmetresitcagan@posta.mu.edu.tr>
2022-10-15 21:56:37 +0300

    week4

---
+++ week4/TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnTest = findViewById(R.id.btnTest);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("bilgisayar.mu.edu.tr/"));
+                startActivity(intent);
+            }
+        });
+    }
+}
